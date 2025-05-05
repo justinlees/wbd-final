1d8bf38a7a837851fe7d4662b4a5732aadbb5ba4
daito5z <parzival124567@gmail.com>
2025-05-05 23:47:26 +0530

    uC11

--- backend/routes/controllers/admin.js
+++ backend/routes/controllers/admin.js
@@ -6,7 +6,7 @@ const collectionC = require("../../model/Cmodel");
 const collectionMsg = require("../../model/messages");
 
 const adminAuth = (req, res) => {
-  const token = req.headers["authorization"].split(" ")[1];
+  const token = req.headers["authorization"];
 
   if (!token) {
     return res.status(403).json({ message: "Token required" });
